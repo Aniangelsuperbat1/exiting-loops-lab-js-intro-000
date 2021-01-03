c085fea17d2f58cb9e4710e87cdab13043f79a4c
Patrick Zhen <zhen.patrick@gmail.com>
2021-01-03 23:48:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+let breakOut = (array, changeValue, stopValue) =>{
+  for (let i = 0; i < array.length; i++){
+    array[i] = changeValue
+    if (array[i] === stopValue){
+      return array
+      break
+    }
+  }
+}
